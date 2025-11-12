f5dcb35c087c7bfddd7bc1f5e6cbde1a963ccd9d
riturajhacker <kumarrituraj2008@gmail.com>
2025-11-13 01:17:53 +0530

    Create App.jsx
    मैं रितुराज कुमार हूँ — एक ऐप और वेब डेवलपर, जिसे साइबर सुरक्षा का ज्ञान है। मेरी सोच है कि तकनीक और सुरक्षा को मिलाकर भारत को नई ऊँचाइयों तक पहुँचाया जाए। मेरा लक्ष्य है कि भारत को AI और सुरक्षित तकनीक में अग्रणी बनाया जाए।
    
    📧 Email: kumarrituraj2008@gmail.com 📞 Phone: 8521574912 📱 Social Media: Mr.hacker_2008

---
+++ App.jsx
@@ -0,0 +1,15 @@
+// client/src/App.jsx
+import React from 'react';
+import Navbar from './components/Navbar';
+import Playground from './components/Playground';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Playground />
+    </div>
+  );
+}
+
+export default App;
